178e1858ad4f1e294d9c866bb97dd9370cb43e78
AnthonyCAS <amtony510@gmail.com>
2024-07-16 12:26:31 -0500

    implement a custom AspectLazyVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -49,46 +49,14 @@ struct EmojiMemoryGameView: View {
     }
 
     private var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = computeGridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                aspectRatio: cardAspectRatio
-            )
-            LazyVGrid(
-                columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)],
-                spacing: 0
-            ) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(cardAspectRatio, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+        AspectLazyVGrid(viewModel.cards, aspectRatio: cardAspectRatio) { card in
+            CardView(card)                
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }
-            .foregroundColor(viewModel.themeColor)
         }
-    }
-
-    private func computeGridItemWidthThatFits(
-        count: Int,
-        size: CGSize,
-        aspectRatio: CGFloat
-    ) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return width.rounded(.down)
-            }
-            columnCount += 1
-        } while columnCount < count
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+        .foregroundColor(viewModel.themeColor)
     }
 }
 
